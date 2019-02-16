8f11f8938b1c3017638a239825197205f479e970
vitvlkv <vitvlkv@gmail.com>
2019-02-16 15:16:25 +0300

    Added more tests

--- src/test/java/ru/vitvlkv/collections/ThreeStacksSortTests.java
+++ src/test/java/ru/vitvlkv/collections/ThreeStacksSortTests.java
@@ -11,6 +11,14 @@ import java.util.Deque;
 public class ThreeStacksSortTests {
 
     @Test
+    public void threeStacksSort7ElemsTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 5, 1, 7, 3, 8, 4));
+        ThreeStacksSort.sortStack(stack);
+
+        Assert.assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 7, 8}, stack.toArray());
+    }
+
+    @Test
     public void threeStacksSort8ElemsTest() {
         Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 5, 1, 7, 3, 8, 4, 7));
         ThreeStacksSort.sortStack(stack);
@@ -19,13 +27,36 @@ public class ThreeStacksSortTests {
     }
 
     @Test
-    public void threeStacksSort7ElemsTest() {
-        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 5, 1, 7, 3, 8, 4));
+    public void threeStacksSort9ElemsTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 14, 5, 1, 7, 3, 8, 4, 7));
         ThreeStacksSort.sortStack(stack);
 
-        Assert.assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 7, 8}, stack.toArray());
+        Assert.assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 7, 7, 8, 14}, stack.toArray());
     }
 
+    @Test
+    public void threeStacksSort10ElemsTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 14, 5, 1, 11, 7, 3, 8, 4, 7));
+        ThreeStacksSort.sortStack(stack);
+
+        Assert.assertArrayEquals(new Integer[] {1, 2, 3, 4, 5, 7, 7, 8, 11, 14}, stack.toArray());
+    }
+
+    @Test
+    public void threeStacksSort11ElemsTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 14, 5, 1, 1, 11, 7, 3, 8, 4, 7));
+        ThreeStacksSort.sortStack(stack);
+
+        Assert.assertArrayEquals(new Integer[] {1, 1, 2, 3, 4, 5, 7, 7, 8, 11, 14}, stack.toArray());
+    }
+
+    @Test
+    public void threeStacksSort12ElemsTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(2, 14, 5, 1, 1, 11, 42, 7, 3, 8, 4, 7));
+        ThreeStacksSort.sortStack(stack);
+
+        Assert.assertArrayEquals(new Integer[] {1, 1, 2, 3, 4, 5, 7, 7, 8, 11, 14, 42}, stack.toArray());
+    }
 
     @Test
     public void threeStacksSortEmptyTest() {
@@ -33,5 +64,13 @@ public class ThreeStacksSortTests {
         ThreeStacksSort.sortStack(stack);
 
         Assert.assertArrayEquals(new Integer[] {}, stack.toArray());
+    }
+
+    @Test
+    public void threeStacksSort1ElemTest() {
+        Deque<Integer> stack = new ArrayDeque<>(Arrays.asList(42));
+        ThreeStacksSort.sortStack(stack);
+
+        Assert.assertArrayEquals(new Integer[] {42}, stack.toArray());
     }
 }
